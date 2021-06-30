75a45e01d171e897069620722d2eabae2e43c0a3
marcoche88 <marco.che@hotmail.it>
2021-06-30 15:33:09 +0200

    pwd generator

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// variabili
+
+var firstName = document.getElementById("name");
+var lastName = document.getElementById("surname");
+var color = document.getElementById("color");
+var password = document.getElementById("pass");
+var numberRandom = 21;
+
+console.log(firstName, lastName, color);
+
+// nome utente
+
+var userName = prompt("Inserisci il tuo nome");
+console.log("userName: " + userName);
+firstName.innerText = "Nome: " + userName; 
+
+// cognome utente
+
+var userSurname = prompt("Inserisci il tuo cognome");
+console.log("userSurname: " + userSurname);
+lastName.innerText = "Cognome: " + userSurname;
+
+// colore preferito
+
+var favoriteColor = prompt("Inserisci il tuo colore preferito");
+console.log("favoriteColor: " + favoriteColor);
+color.innerText = "Colore preferito: " + favoriteColor;
+
+// genera password
+
+password.innerText = "Password: " + userName + userSurname + favoriteColor + 21;
